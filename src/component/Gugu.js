6c2e998904bb8592e9bf938daf1cb8606bc2697a
Gukss <dongkuk97@naver.com>
2022-01-17 14:13:17 +0900

    react functional component gugudan

---
+++ src/component/Gugu.js
@@ -0,0 +1,44 @@
+import { useState } from "react";
+
+const Gugu = () => {
+    const [number, setNumber] = useState({
+        num1: Math.ceil(Math.random() * 9),
+        num2: Math.ceil(Math.random() * 9),
+    });
+    const [answer, setAnswer] = useState();
+    const [result, setResult] = useState("땡");
+
+    const onChangeAnswer = (e) => {
+        setAnswer(e.target.value);
+    };
+
+    const onSubmitAnswer = (e) => {
+        e.preventDefault();
+        if (number.num1 * number.num2 === parseInt(answer)) {
+            setResult("정답");
+            setAnswer("");
+            setNumber({
+                num1: Math.ceil(Math.random() * 9),
+                num2: Math.ceil(Math.random() * 9),
+            });
+        } else {
+            setResult("땡");
+            setAnswer("");
+        }
+    };
+
+    return (
+        <div>
+            <div>
+                {number.num1} 곱하기 {number.num2}는?
+            </div>
+            <form onSubmit={onSubmitAnswer}>
+                <input type="number" value={answer} onChange={onChangeAnswer} />
+                <button>제출</button>
+            </form>
+            <div>{result}</div>
+        </div>
+    );
+};
+
+export default Gugu;
